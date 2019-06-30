38ad4d83d1c1680d310713a4340409b0b4d23b1f
Jordan Pittier <jordan.pittier@gmail.com>
2019-06-29 20:59:32 -0700

    Bump dependenciers version

--- scripts/docker-build-push-prod-img.sh
+++ scripts/docker-build-push-prod-img.sh
@@ -5,14 +5,14 @@ set -o pipefail
 #export VERSION=$(git describe --tags --always --dirty)  --- fail because git is not installed in runner image
 
 docker pull $CI_REGISTRY_IMAGE:build-$VERSION
-docker pull $CI_REGISTRY_IMAGE:latest || true
+docker pull $CI_REGISTRY_IMAGE:run-latest || true
 
 docker build --pull \
     --cache-from=$CI_REGISTRY_IMAGE:build-$VERSION \
-    --cache-from=$CI_REGISTRY_IMAGE:latest \
-    -t $CI_REGISTRY_IMAGE:$VERSION \
-    -t $CI_REGISTRY_IMAGE:latest \
+    --cache-from=$CI_REGISTRY_IMAGE:run-latest \
+    -t $CI_REGISTRY_IMAGE:run-$VERSION \
+    -t $CI_REGISTRY_IMAGE:run-latest \
     .
 
-docker push $CI_REGISTRY_IMAGE:$VERSION
-docker push $CI_REGISTRY_IMAGE:latest
+docker push $CI_REGISTRY_IMAGE:run-$VERSION
+docker push $CI_REGISTRY_IMAGE:run-latest
